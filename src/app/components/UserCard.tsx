83625a4b5038e94af5dc432ff6d140fb998aba6e
anaswara rajesh <anaswara.rajesh@lanwaresolutions.com>
2024-07-27 15:16:36 +0530

    fetch exstra10 users when click load more button

--- src/app/components/UserCard.tsx
+++ src/app/components/UserCard.tsx
@@ -1,8 +1,8 @@
 export default function UserCard({ user }: any) {
   return (
     <div className="bg-indigo-400 text-white rounded w-96 p-3">
-      <p>{`${user.name.first} ${user.name.last}`}</p>
-      <img src={user.picture.thumbnail} alt="User Thumbnail" />
+      <p>{`${user?.name?.first} ${user?.name?.last}`}</p>
+      <img src={user?.picture?.thumbnail} alt="User Thumbnail" />
       <div>{user?.phone}</div>
       <div>{user?.email}</div> 
     </div>
--- src/app/components/UsersList.tsx
+++ src/app/components/UsersList.tsx
@@ -1,16 +1,27 @@
 "use client";
 import React, { useState } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "../actions/getUsers";
+
+const NUMBER_OF_USERS = 10;
 
 const UsersList = ({ initialUsers }: any) => {
   const [users, setUsers] = useState(initialUsers);
   console.log(initialUsers, "initialUsers");
+
+  const fetchMoreUsers = async () => {
+    const result = await getUsers(NUMBER_OF_USERS);
+    const fetchedUsers = result?.results;
+    console.log(fetchedUsers,"fetchedUsers");
+    setUsers([...users, ...fetchedUsers]);
+  };
 
   return (
     <div className="flex flex-col gap-3">
       {users?.map((user: any, index: any) => (
         <UserCard key={index} user={user} />
       ))}
+      <button onClick={fetchMoreUsers}>Load More</button>
     </div>
   );
 };
