012ce7775b1a2dbcb8bc1e3b50a0806a4895629a
anaswara rajesh <anaswara.rajesh@lanwaresolutions.com>
2024-07-27 15:30:34 +0530

    adding max-limit to load

--- src/app/components/UsersList.tsx
+++ src/app/components/UsersList.tsx
@@ -5,16 +5,23 @@ import { getUsers } from "../actions/getUsers";
 import { useInView } from "react-intersection-observer";
 
 const NUMBER_OF_USERS = 10;
+const MAX_USERS = 100;
 
 const UsersList = ({ initialUsers }: any) => {
   const [users, setUsers] = useState(initialUsers);
   const { ref, inView } = useInView();
+  const [isFetching, setIsFetching] = useState(false);
+
   console.log(initialUsers, "initialUsers");
 
   const fetchMoreUsers = async () => {
+    if (users?.length >= MAX_USERS || isFetching) return;
+    setIsFetching(true);
+
     const result = await getUsers(NUMBER_OF_USERS);
-    const fetchedUsers = result?.results;
-    setUsers([...users, ...fetchedUsers]);
+    const fetchedUsers = result?.results || [];
+    setUsers((prevUsers: any[]) => [...prevUsers, ...fetchedUsers]);
+    setIsFetching(false);
   };
 
   useEffect(() => {
@@ -25,10 +32,10 @@ const UsersList = ({ initialUsers }: any) => {
 
   return (
     <div className="flex flex-col gap-3">
-      {users?.map((user: any, index: any) => (
+      {users?.map((user: any, index: number) => (
         <UserCard key={index} user={user} />
       ))}
-      <div ref={ref}>Loading...</div>
+      {users?.length < MAX_USERS && <div ref={ref}>Loading...</div>}
     </div>
   );
 };
